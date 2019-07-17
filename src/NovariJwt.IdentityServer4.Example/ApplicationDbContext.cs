73b1c9eadbc5e4b792eea41350c55742331e3dd9
Leonardo Suarez <elionxl900@gmail.com>
2019-07-17 15:13:38 -0500

    initial

---
+++ src/NovariJwt.IdentityServer4.Example/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NovariJwt.IdentityServer4.Example
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
